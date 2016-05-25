a85c22f5265c244d5f93b135680617b7b2facb43
jcortes <jacortesmahmud@gmail.com>
2016-05-24 22:18:13 -0500

    On branch 03-shopaid

--- app/App.js
+++ app/App.js
@@ -1,10 +1,11 @@
 var React = require('react');
 var ReactDOM = require('react-dom');
 var Router = require('react-router').Router;
+var hashHistory = require('react-router').hashHistory;
 var routes = require('./config/routes');
 
 ReactDOM.render(
-  <Router>
+  <Router history={hashHistory}>
     {routes}
   </Router>,
   document.querySelector("#app")
