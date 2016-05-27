9238a81207894dc37fc06cca1576ffe180327e53
jcortes <jacortesmahmud@gmail.com>
2016-05-26 23:37:51 -0500

    On branch 03-shopaid

--- app/config/routes.js
+++ app/config/routes.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import {Route, IndexRoute} from 'react-router';
 import Main from '../components/Main';
-import SAPersonLogin from '../components/PersonLogin';
+import SAPersonLogin from '../components/SAPersonLogin';
 import Demo1 from '../components/Demo1';
 
 export default (
